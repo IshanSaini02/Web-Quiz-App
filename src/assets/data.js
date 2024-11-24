486a9542fb808265905d47bebd4a28fb67e49f7e
Ishan Saini <ishansaini382@gmail.com>
2024-11-24 12:51:48 +0530

    "initial commit"

---
+++ src/assets/data.js
@@ -0,0 +1,164 @@
+export const data = [
+    {
+        question : 'Javascript is an ____ language?',
+        option1 : 'Object-Based',
+        option2 : 'Procedural',
+        option3 : 'Object-Oriented',
+        option4 : 'None of the above',
+        ans : 1
+    },
+    {
+        question : 'Inside which HTML element do we put the JavaScript?',
+        option1 : '<script>',
+        option2 : '<javascript>',
+        option3 : '<scripting>',
+        option4 : '<js>',
+        ans : 1
+    },
+    {
+        question : 'How do we write a comment in javascript?',
+        option1 : '/* */',
+        option2 : '//',
+        option3 : '#',
+        option4 : '$$',
+        ans : 2
+    },
+    {
+        question : 'The "let" and " var" are known as:',
+        option1 : 'Prototypes',
+        option2 : 'Declaration statements',
+        option3 : 'Data Types',
+        option4 : 'Keywords',
+        ans : 3
+    },
+    {
+        question : 'What does NaN means?',
+        option1 : 'Negative Number',
+        option2 : 'Not a Number',
+        option3 : 'Both A & B',
+        option4 : 'None of the above',
+        ans : 2
+    },
+    {
+        question : '____ is Not a pop box in javaScript?',
+        option1 : 'Confirm',
+        option2 : 'Error',
+        option3 : 'Alert',
+        option4 : 'Prompt',
+        ans : 2
+    },
+    {
+        question : 'What keyword is used to declare an asynchronous function in Javascript?',
+        option1 : 'async',
+        option2 : 'await',
+        option3 : 'setTimeout',
+        option4 : 'None of the above',
+        ans : 1
+
+    },
+    {
+        question : 'What keyword is used to check whether a given property is valid or not?',
+        option1 : 'in',
+        option2 : 'is in',
+        option3 : 'exists',
+        option4 : 'lies',
+        ans : 1
+    },
+    
+    {
+        question :'The name of the company that developed javaScript is?',
+        option1 : 'Microsoft',
+        option2 : 'Apple',
+        option3 : 'Netscape',
+        option4 : 'Titanium',
+        ans : 3
+    },
+    {
+        question :'How do you create a function in JavaScript?',
+        option1 : 'function:myFunction()',
+        option2 : 'function = myFunction()',
+        option3 : 'function myFunction()',
+        option4 : 'None of the above',
+        ans : 3
+    },
+    {
+        question :'How do you call a function named "myFunction"?',
+        option1 : 'call function myFunction()',
+        option2 : 'myFunction()',
+        option3 : 'call myFunction()',
+        option4 : 'call myFunction',
+        ans : 2
+    },
+    {
+        question : 'Which event is related to the keyboard?',
+        option1 : 'onclick',
+        option2 : 'onfocus',
+        option3 : 'onkeypress',
+        option4 : 'onload',
+        ans : 3
+    },
+    {
+        question : 'How do you write "Hello World" in an alert box?',
+        option1 : 'msgBox("Hello World!")',
+        option2 : 'msg("Hello World")',
+        option3 : 'alertBox("Hello World")',
+        option4 : 'alert("Hello World")',
+        ans : 4
+    },
+    {
+        question : 'How can a datatype be declared to be a constant type?',
+        option1 : 'const',
+        option2 : 'var',
+        option3 : 'constant',
+        option4 : 'Const',
+        ans : 1
+    },
+    {
+        question : 'Which of the following are closures in Javascript?',
+        option1 : 'Variables',
+        option2 : 'Functions',
+        option3 : 'Objects',
+        option4 : 'All of the above',
+        ans : 4
+    },
+    {
+        question : 'Which on is not a mouse event?',
+        option1 : 'onmousescroller',
+        option2 : 'onclick',
+        option3 : 'onmousemove',
+        option4 : 'onmouseover',
+        ans : 1
+    },
+    {
+        question : 'How can a variable be declared in javaScript?',
+        option1 : 'strName var',
+        option2 : 'val str',
+        option3 : 'var strName',
+        option4 : 'str',
+        ans : 3
+    },
+    {
+        question : 'Which of the following is not a Javascript framework?',
+        option1 : 'Node',
+        option2 : 'Vue',
+        option3 : 'React',
+        option4 : 'Cassandra',
+        ans : 4
+    },
+    {
+        question : 'Which of these is not a keyword?',
+        option1 : 'debugger',
+        option2 : 'with',
+        option3 : 'use strict',
+        option4 : 'if',
+        ans : 3
+    },
+    {
+        question : 'How to stop an interval timer in Javascript?',
+        option1 : 'clearInterval',
+        option2 : 'ClearInterval',
+        option3 : 'clearTime',
+        option4 : 'intervalOver',
+        ans : 1
+    },
+]
